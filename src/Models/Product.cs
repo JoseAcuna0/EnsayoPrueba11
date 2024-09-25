9224ac8b1c86544b701f71d918b1ca49125ef523
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2024-09-25 20:26:31 -0300

    Ahora si

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,18 @@ namespace apiEnsayoNuevo.src.Models
 {
     public class Product
     {
-        
+        public int Id { get; set; }
+
+        public required string Code { get; set; }
+
+
+        [StringLength(100, MinimumLength = 3)]
+        public required string Name { get; set; }
+
+        [RegularExpression(@"POLERAS|PANTALONES|SOMBREROS")]
+        public required string Category { get; set; }
+
+        [Range(1, 99)]
+        public int Stock { get; set; }
     }
 }
