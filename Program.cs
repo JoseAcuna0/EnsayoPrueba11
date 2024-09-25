e8266493b3cbd2ef87349c065b3cf0da50cfdc15
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2024-09-25 20:43:55 -0300

    feat: firstMigration

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
+using apiEnsayoNuevo.src.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") ?? "Data Source";
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlite(connectionString));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDBContext>();
+    await context.Database.MigrateAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
